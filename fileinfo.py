9ef1e18dde12bb36396d3d609ae215ba4d4b3b1f
hmi <hmi@linux.com>
2017-10-23 21:45:29 +0200

    try alignment to pylint

--- fileinfo.py
+++ fileinfo.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
+
 class FileInfo:
     """
     Representation of file information.
     """
 
-    def __init__(self, file_type, file_name, file_checksum, file_size=0, \
-            file_record_count=0):
+    def __init__(self, file_type, file_name, file_checksum, file_size=0,
+                 file_record_count=0):
         self.file_type = file_type
         self.file_name = file_name
         self.file_checksum = file_checksum
         self.file_size = file_size
         self.file_record_count = file_record_count
-
